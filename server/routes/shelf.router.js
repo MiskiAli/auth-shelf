bd70296df41e250fd32d95827a8f01e48dfcfe93
Avery Yang <averyyang2674@gmail.com>
2022-07-27 16:25:32 -0500

    create get router in shelf.router

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,28 +6,44 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
+    if (req.isAuthenticated()) {
+        console.log('IN /shelf');
+        console.log(`isAuthenticated`, req.isAuthenticated());
+        // console.log(`user`, req.user);
+        let queryText = `select * from item`;
+        pool.query(queryText)
+            .then((result) => {
+                res.send(result.rows);
+                console.log(`Our result is:`, result.rows);
+            })
+            .catch((err) => {
+                console.log(`ERR in /shelf router`, err);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 /**
  * Add an item for the logged in user to the shelf
  */
 router.post('/', (req, res) => {
-  // endpoint functionality
+    // endpoint functionality
 });
 
 /**
  * Delete an item if it's something the logged in user added
  */
 router.delete('/:id', (req, res) => {
-  // endpoint functionality
+    // endpoint functionality
 });
 
 /**
  * Update an item if it's something the logged in user added
  */
 router.put('/:id', (req, res) => {
-  // endpoint functionality
+    // endpoint functionality
 });
 
 /**
@@ -35,14 +51,14 @@ router.put('/:id', (req, res) => {
  * they have added to the shelf
  */
 router.get('/count', (req, res) => {
-  // endpoint functionality
+    // endpoint functionality
 });
 
 /**
  * Return a specific item by id
  */
 router.get('/:id', (req, res) => {
-  // endpoint functionality
+    // endpoint functionality
 });
 
 module.exports = router;
