d151e71de26cd45bd4eb253383cc3f825a7293a5
Avery Yang <averyyang2674@gmail.com>
2022-07-27 16:54:20 -0500

    create saga, store, dispatch

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ShelfPage() {
-  return (
-    <div className="container">
-      <h2>Shelf</h2>
-      <p>All of the available items can be seen here.</p>
-    </div>
-  );
+    const items = useSelector((store) => store.item);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_ITEM' });
+    }, []);
+
+    return (
+        <div className="container">
+            <h2>Shelf</h2>
+            <p>All of the available items can be seen here.</p>
+        </div>
+    );
 }
 
 export default ShelfPage;
